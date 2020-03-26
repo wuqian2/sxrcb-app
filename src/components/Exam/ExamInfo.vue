e782a98ab920fcf8de74ed3446586b36771968fc
wuqian2 <1050570474@qq.com>
2020-03-26 10:59:42 +0800

    修改考试详情页面

--- src/components/Exam/ExamInfo.vue
+++ src/components/Exam/ExamInfo.vue
@@ -1,44 +1,48 @@
 <template>
     <div class="container" id="test">
-        <van-nav-bar
-                title="xxx练习题"
-                fixed
-                left-arrow
-                @click-left="back"
-        >
+        <van-nav-bar  title="xxx练习题" fixed left-arrow   @click-left="back" >
             <template #right>
                 <div style="color: white">
                     <van-count-down style="color: white;line-height: 40px" :time="time"/>
                 </div>
-
             </template>
-
         </van-nav-bar>
 
 
         <div id="topicPanel">
-            <van-panel  :title="topicTitle" style="padding: 0 10px 40px 10px;">
-             <van-radio-group v-model="topic.answer">
-                <van-radio v-for="option in topic.options" :key="option.optionId" :name="option.optionId" class="radio">{{option.content}}</van-radio>
+            <van-panel :title="topicTitle" style="padding: 0 10px 40px 10px;">
+                <!--单选框-->
+                <van-radio-group v-show="topic.type === '01'" v-model="topic.answer">
+                    <van-radio v-for="option in topic.options" :key="option.optionId"
+                               :name="option.optionId" class="radio">
+                                {{option.content}}
+                    </van-radio>
+                </van-radio-group>
 
-             </van-radio-group>
-         </van-panel>
+                <!--复选框-->
+                <van-checkbox-group v-show="topic.type === '02'"  v-model="topic.answerArray">
+                    <van-checkbox shape="square" v-for="option in topic.options" :key="option.optionId"
+                                  :name="option.optionId">{{option.content}}</van-checkbox>
+                </van-checkbox-group>
 
+            </van-panel>
         </div>
 
         <van-tabbar v-model="active">
             <van-tabbar-item icon="arrow-left" @click="lastTopic">上一题</van-tabbar-item>
-            <van-tabbar-item icon="description" @click="showList">查看</van-tabbar-item>
+            <van-tabbar-item icon="description" @click="showList">答题卡</van-tabbar-item>
             <van-tabbar-item icon="arrow" @click="nextTopic">下一题</van-tabbar-item>
         </van-tabbar>
 
 
-        <van-popup
-                v-model="showFlag"
-                position="bottom"
-                :style="{ height: '30%' }"
-        >
-
+        <van-popup  v-model="showFlag"  position="bottom"  :style="{ height: '60%' }" >
+            <van-panel title="答题板">
+                <van-grid :column-num="6" :border="false" clickable>
+                    <van-grid-item  v-for="(value,index) in examData.length"  :key="value"  square  @click="goIndex(index)" >
+                        <van-tag round plain :color="(examData[index].answer || (examData[index].answerArray && examData[index].answerArray.length > 0 )) ? '#07c160':'#000000'"  type="primary">{{value}}</van-tag>
+                    </van-grid-item>
+                </van-grid>
+            </van-panel>
         </van-popup>
     </div>
 </template>
@@ -50,8 +54,6 @@
     import Velocity from 'velocity-animate';
     import 'velocity-animate/velocity.ui.js';
 
-
-
     export default {
         name: "Index",
         data: function () {
@@ -61,7 +63,8 @@
                 showFlag: false,
                 topicIndex: 0,
                 topic: {},
-                visible: true
+                visible: true,
+                examData: examData
             }
         },
         methods: {
@@ -71,6 +74,12 @@
             showList: function () {
                 // 查看答题情况
                 this.showFlag = true
+            },
+            goIndex: function (index) {
+                // 答题版导航
+                this.topic = examData[index];
+                this.topicIndex = index;
+                this.showFlag = false;
             },
             nextTopic: function () {
                 // 下一题
@@ -143,6 +152,10 @@
     }
 
     .radio {
+        margin-top: 20px;
+    }
+
+    .van-checkbox{
         margin-top: 20px;
     }
 
